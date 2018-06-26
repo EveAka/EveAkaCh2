cef8b1adef156c57f364b7f1d92170574763ee34
EveAka <eveaka24@gmail.com>
2018-06-27 01:17:53 +0300

    new updates

--- rides.py
+++ rides.py
@@ -19,17 +19,26 @@ rides = [
         'id': 2,
         'NoPLate': u'UBB214G',
         'destination': u'Mukono', 
-        'done': False
+      
+    },
+    {
+        'id': 3,
+        'NoPLate': u'UBC235H',
+        'destination': u'Mukono', 
+
     }
+    
 
 ]
 
 requests=[
     {
-        'id': 1,
+       'id': 1,
         'ride_id': 1,
-        'destination': u'Makindye'
+        'destination': u'Makindye',
+        'NoPLate': u'UBA234F'
     }
+   
 ]
 
 #This the GET method to fetch all rides
@@ -45,7 +54,7 @@ def get_ride(ride_id):
         abort(404)
     return jsonify({'ride': ride[0]})   
 
-#This is POST method to edit a ride
+#This is POST method to create a new ride
 @app.route('/ridesapp/api/v1/rides', methods=['POST'])
 def create_ride():
     if not request.json or not 'NoPLate' in request.json:
@@ -53,11 +62,91 @@ def create_ride():
     ride = {
         'id': rides[-1]['id'] + 1,
         'NoPLate': request.json['NoPLate'],
-        'destination': request.json.get('destination'),
-        'done': False
+        'destination': request.json.get('destination')
+        
     }
     rides.append(ride)
     return jsonify({'ride': ride}), 201
+
+#method to edit a ride
+@app.route('/ridesapp/api/v1/rides', methods=['PUT'])
+def update_ride(ride_id):
+    ride = [ride for ride in ride if ride['id'] == ride_id]
+    if len(ride) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    ride[0]['NoPLate'] = request.json.get('NoPLate', ride[0]['NoPLate'])
+    ride[0]['destination'] = request.json.get('destination', ride[0]['destination'])
+    return jsonify({'ride': ride[0]})
+    
+#This is a DELETE Method
+@app.route('/ridesapp/api/v1/rides/<int:ride_id>', methods=['DELETE'])
+def delete_ride(ride_id):
+    ride = [ride for ride in rides if ride['id'] == ride_id]
+    if len(ride) == 0:
+        abort(404)
+    rides.remove(ride[0])
+    return jsonify({'result': True}) 
+
+#Write a method here to create a request 
+@app.route('/ridesapp/api/v1/rides/<int:ride_id>/requests', methods=['POST'])
+def create_request(ride_id):
+    ride = [ride for ride in rides if ride['id'] == ride_id]
+    if ride :
+    
+        request = {
+            'id': requests[-1]['id'] + 1,
+            'destination': flask.request.json.get('destination'),
+            'ride_id': ride_id
+        
+        }
+        requests.append(request)
+        return jsonify({'request': request}), 201
+    else:
+        return("Ride Id doesnt exist")    
+#Write a method here to get all requests
+@app.route('/ridesapp/api/v1/requests', methods=['GET'])
+def get_all_requests():
+    return jsonify({'requests': requests})
+
+#Write a method here to get a single request
+@app.route('/ridesapp/api/v1/requests/<int:request_id>', methods=['GET'])
+def get_a_single_request(request_id):
+    my_req = [req for req in requests if req['id'] == request_id] # loop through the requests to check for the requestid
+    if len(my_req) == 0:
+        abort(404)
+    return jsonify({'request': my_req[0]})
+
+#Write a method here to update a request 
+@app.route('/ridesapp/api/v1/requests/<int:request_id>', methods=['PUT'])
+def update_request(request_id):
+    my_req = [req for req in requests if req['id'] == request_id]
+    if len(my_req) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'NoPlate' not in request.json:
+        abort(400)
+    my_req[0]['NoPlate'] = request.json.get('NoPlate', my_req[0]['NoPlate'])
+    return jsonify({'request': my_req[0]})  
+
+#method to delete a request 
+@app.route('/ridesapp/api/v1/requests/<int:request_id>', methods=['DELETE'])
+def delete_request(request_id):
+    my_req = [req for req in requests if req['id'] == request_id]
+    if len(my_req) == 0:
+        abort(404)
+    requests.remove(my_req[0])
+    return jsonify({'request': my_req[0]})
+
+#method get all requests by a particular id 
+@app.route('/ridesapp/api/v1/rides/<int:ride_id>/requests', methods=['GET'])
+def get_all_requests_by_ride_id(ride_id):
+    my_req = [req for req in requests if req['ride_id'] == ride_id] # loop through the requests to check for the requestid
+    if len(my_req) == 0:
+        abort(404)
+    return jsonify({'request': my_req})
 
 
 #This is an error handler to give a better response for code 404!
--- tests.py
+++ tests.py
@@ -8,16 +8,16 @@ class TestFlaskApi(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client()
         
-    def test_get_rides(self):
+    def test_get_all_rides(self):
             res = self.client.get('/ridesapp/api/v1/rides')           
             result = json.loads(res.data.decode())          
             self.assertEqual(result['rides'][0]['NoPLate'], "UBA234F")
             self.assertEqual(res.status_code, 200)
 
-    def test_get_single_ride(self):
-            res = self.client.get('/ridesapp/api/v1/rides/3') 
+    def test_get_a_single_ride(self):
+            res = self.client.get('/ridesapp/api/v1/rides/1') 
             result = json.loads(res.data.decode())
-            self.assertEqual(result['ride']['NoPLate'], "UBC235H")
+            self.assertEqual(result['ride']['NoPLate'], "UBA234F")
             self.assertEqual(res.status_code, 200)        
 
     def test_get_ride_not_found(self):
@@ -26,8 +26,98 @@ class TestFlaskApi(unittest.TestCase):
             result = json.loads(res.data.decode())
             self.assertEqual(result['error'], "Not found")
             self.assertEqual(res.status_code, 404)
-                   
+
+
+    def test_create_ride(self):  
+
+            ride =  {
+        'id': 1,
+        'NoPLate': u'UBA234F'
+        }
+                          
+            res = self.client.post('/ridesapp/api/v1/rides', data = json.dumps(ride),content_type = 'application/json')
+            result = json.loads(res.data.decode())
+            print(result)
+            data = json.loads(res.get_data())
+            self.assertEqual(data['ride']['NoPLate'], "UBA234F")
+            self.assertEqual(res.status_code, 201)
+
+#write a test on update ride
+    def test_update_ride(self):
+        
+            
+            ride ={
+        'id': 2,
+        'NoPLate': u'UBB214G',
+        'destination': u'Mukono'
+      
+    }
+            res = self.client.put('ridesapp/api/v1/rides/2',data=json.dumps(ride),content_type='application/json')                          
+            self.assertEqual(res.status_code, 200)
+            data = json.loads(res.get_data())
+            self.assertEqual(data['ride']['NoPLate'], 'UBB214G')
+            self.assertEqual(ride[2]['NoPLate'], 'UDE114D') 
     
+#test on delete ride
+    def test_delete_ride(self):
+            res = self.client.delete('/ridesapp/api/v1/rides/3')#Good URL
+            self.assertEqual(res.status_code, 200)
+            res = self.client.delete('/ridesapp/api/v1/rides')#Bad URL
+            self.assertEqual(res.status_code, 405)
+
+#test on create request
+    def test_create_request(self):  
+
+            request =  {
+                   
+        'id': 1,
+        'ride_id':1,
+        'NoPLate': u'UBA234F',
+        
+        }
+                       
+            
+            res = self.client.post('ridesapp/api/v1/requests', data = json.dumps(request),content_type = 'application/json')
+            result = json.loads(res.data.decode())
+            print(result)
+            data = json.loads(res.get_data())
+            self.assertEqual(data['ride']['NoPLate'], "UBA234F")
+            self.assertEqual(res.status_code, 201)
+    
+#test on get all requests
+    def test_get_all_requests(self):
+            res = self.client.get('/ridesapp/api/v1/requests')           
+            result = json.loads(res.data.decode())   
+            print(result)       
+            self.assertEqual(res.status_code, 200)
+
+#test on get a single request
+    def test_get_a_single_request(self):
+            res = self.client.get('/ridesapp/api/v1/requests/1') 
+            result = json.loads(res.data.decode())
+            print(result)
+            self.assertEqual(res.status_code, 200)        
+
+
+#test on update request
+
+#test on delete a request
+def test_delete_request(self):
+            res = self.client.delete('/ridesapp/api/v1/rides/request/1')#Good RideURL
+            self.assertEqual(res.status_code, 200)
+            
+            res = self.client.delete('/ridesapp/api/v1/rides')#Bad RideURL
+            self.assertEqual(res.status_code, 405)
+
+   
+
+
+
+    
+  
+
+
+
         
                
         
